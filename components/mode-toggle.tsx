69d6cedd86cfaecb0fb86c1222d3a2e47e91ef62
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-20 21:40:26 +0800

    refactor: Update ModeToggle component to use useEffect for mounting

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,19 +1,29 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
 import { BsSunFill } from "react-icons/bs";
 import { FaMoon } from "react-icons/fa6";
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <Button
       variant={"ghost"}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      aria-label="Toggle Dark Mode"
+      className="transition-colors duration-300"
     >
-      {theme === "dark" ? (
+      {resolvedTheme === "dark" ? (
         <BsSunFill className="text-yellow-500" size={20} />
       ) : (
         <FaMoon className="text-yellow-500" size={20} />
